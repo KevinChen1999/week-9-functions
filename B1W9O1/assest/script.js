ba3bb77bd14373289e05bd7e8946d9cc0d6f1a21
Ninja1999 <99059352@mydavinci.nl>
2020-10-23 03:18:25 +0200

    WIP

---
+++ B1W9O1/assest/script.js
@@ -0,0 +1,24 @@
+const PRICEBITTERBALLEN8 =
+const  PRICEBITTERBALLEN19 =
+
+
+
+function orderdrink(){
+	var drank = prompt("wat wilt u te drinken? \nwe hebben Fris, Wijn, Bier.");
+}
+
+
+
+
+
+function ordersnacks(){}
+	var snack = prompt("hoeveel bitterballen wilt u? \n8 of 16 stuks bitterballen?");
+
+
+
+
+function recipt(){}
+
+
+
+
