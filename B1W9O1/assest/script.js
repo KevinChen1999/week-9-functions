9cf873fc57a28e517226e10f612f6b497facf02c
Ninja1999 <99059352@mydavinci.nl>
2020-11-01 21:34:24 +0100

    functions done

--- B1W9O1/assest/script.js
+++ B1W9O1/assest/script.js
@@ -1,24 +1,61 @@
-const PRICEBITTERBALLEN8 =
-const  PRICEBITTERBALLEN19 =
+var fris = "";
+var bier = "";
+var wijn = "";
+var bitterballen = "";
+var count = true;
+var receipt = document.getElementById("receipt")
+
+const prijsFris = 200;
+const prijsBier = 300;
+const prijsWijn = 400;
+const prijsSnack = 600;
 
 
 
-function orderdrink(){
-	var drank = prompt("wat wilt u te drinken? \nwe hebben Fris, Wijn, Bier.");
+function takeorder(){
+	while(count == true){
+		var order = prompt("Wat wilt u bestellen?. U kunt kiezen tussen fris bier of wijn als drank of portie bitterballen");
+		if (order == "fris"){
+			fris += Number (prompt("Hoeveel fris wil je?"))
+		}
+		else if (order == "bier"){
+			bier += Number (prompt("Hoeveel bier wil je?"))
+		}
+		else if (order == "wijn"){
+			wijn += Number (prompt("Hoeveel wijn wil je?"))
+		}
+		else if (order == "bitterballen"){
+			bitterballen += Number (prompt("Hoeveel bitterballen wilt u?  8 of 16 porties?"))
+		}
+		else if (order == "stop"){
+			alert("Bedankt voor uw bestelling. \nals u de rekening wilt, druk op de kop 'reciept'");
+			count = false
+		}
+		else{
+			alert("invalid argument");
+	}
+
+	}
+	
 }
 
 
 
 
+function OrderTotal(){
+	var totalPrice = bier*prijsBier + wijn*prijsWijn + fris*prijsFris + bitterballen*prijsSnack;
+    var receiptBuilder = "";
 
-function ordersnacks(){}
-	var snack = prompt("hoeveel bitterballen wilt u? \n8 of 16 stuks bitterballen?");
+    receiptBuilder += "<h1> Dining Hall cafe </h1>";
+    receiptBuilder += "Aantal bier: " + bier +" totaal prijsbier: &euro;"+ bier*(prijsBier/100)+ "<br>";
+    receiptBuilder += "Aantal wijn: " + wijn +" totaal prijswijn: &euro;"+ wijn*(prijsWijn/100)+ "<br>";
+    receiptBuilder += "Aantal fris: " + fris +" totaal prijsfris: &euro;"+ fris*(prijsFris/100)+ "<br>";
+    receiptBuilder += "Aantal bitterballen: " + bitterballen +" totaal prijsSnack: &euro;"+ bitterballen*(prijsSnack/100)+ "<br>";
+    receiptBuilder += "----------------------------------- + <br>";
+    receiptBuilder += "Subtotaal: &euro; " + (totalPrice/100);
 
-
-
-
-function recipt(){}
-
+    receipt.innerHTML =  receiptBuilder ;
+}
 
 
 
---
+++ Lab1/Opdracht 1/Opdracht1.js
@@ -0,0 +1,6 @@
+function HelloWorld(wld){
+	for (var i = 0 ; i < wrd ; i++) {
+		0 ument.write("Hello World!!!")
+}
+
+HelloWorld(3);
